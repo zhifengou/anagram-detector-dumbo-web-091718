1bbb19762b8f78b9100672f005010e6451132019
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 16:43:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end 
   
   def match(find_anagram_in_this_string) 
-    # puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+   
     find_anagram_in_this_string.find_all do |word| 
       if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
